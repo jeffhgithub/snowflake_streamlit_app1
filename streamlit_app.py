5de6d385fadb1dcb105dae925af68166a0110758
Jeff Hanson <61760324+jeffhgithub@users.noreply.github.com>
2023-10-09 14:11:28 -0700

    Update streamlit_app.py
    modified header and menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
-
-streamlit.header('Breakfast Menu')
-streamlit.text ('Omega 3 & Blueberry Oatmeal')
-streamlit.text ('Kale, Spinach & Rocket Smoothe')
-streamlit.text ('Hard-Boile Free-Range Egg')
+streamlit.title('My Mom's New Healthy Diner')
+   
+streamlit.header('Breakfast Favorites')
+streamlit.text ('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text ('🥗 Kale, Spinach & Rocket Smoothe')
+streamlit.text ('🐔 Hard-Boile Free-Range Egg')
+streamlit.text ('🥑🍞 Avacado Toast')
