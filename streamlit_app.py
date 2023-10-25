c6ddb4deb8ea4c494ca3f800bb51d5ac82f17e02
Jeff Hanson <61760324+jeffhgithub@users.noreply.github.com>
2023-10-24 18:54:18 -0700

    Update streamlit_app.py
    I hope I got this right!! :-(

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,7 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title('My Moms New Healthy Diner')
-   
+streamlit.title('My Moms New Healthy Diner')   
 streamlit.header('Breakfast Favorites')
 streamlit.text ('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text ('🥗 Kale, Spinach & Rocket Smoothie')
@@ -57,8 +56,16 @@ if streamlit.button('Get Fruit Load List'):
 # don't run anything past here while we troubleshoot
 #streamlit.stop()
 
-fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', fruit_choice)
+# Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+   with my_cnx.cursor() as my_cur:
+      my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+      return "Thanks for adding " + new_fruit
+add_my_fruit =  streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+      back_from_function = insert_row_snowflake(add_my_fruit)
+      streamlit.text(back_from_function)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
 
